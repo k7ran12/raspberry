c9a5c52bb6f19370adb57dc96a3653d12f8a803f
Cristian Antonio Valverde <cristian.antonio@datasys.la>
2024-02-23 19:52:03 -0500

    add

--- dev.py
+++ dev.py
@@ -102,6 +102,21 @@ while True:
     if wlan.isconnected():
         print("Sending GET request...")
         getData()
+        if time.ticks_diff(time.ticks_ms(), ultimo_envio) >= 10000:
+            contador = cargar_contador()
+            print("Cada 10 segundos")
+            data = {
+                "verificacion": "Conex1on",
+                "qr": "",
+                "idBus": "",
+                "accion": "",
+                "fecha": "2024-02-22T18:29:49.368",
+                "entradas": contador,
+                "salidas": 0
+            }
+            response = requests.post(url, json=data)
+            if response.status_code == 200:
+                ultimo_envio = time.ticks_ms()
     else:
         print("Lost connection to Wi-Fi. Attempting to reconnect...")
         wlan.disconnect()
@@ -110,21 +125,5 @@ while True:
             getData()
             print("Failed to reconnect. Retrying...")
             time.sleep(5)
-    
-    if time.ticks_diff(time.ticks_ms(), ultimo_envio) >= 10000:
-        contador = cargar_contador()
-        print("Cada 10 segundos")
-        data = {
-            "verificacion": "Conex1on",
-            "qr": "",
-            "idBus": "",
-            "accion": "",
-            "fecha": "2024-02-22T18:29:49.368",
-            "entradas": contador,
-            "salidas": 0
-        }
-        response = requests.post(url, json=data)
-        if response.status_code == 200:
-            ultimo_envio = time.ticks_ms()
 
     time.sleep(1)
